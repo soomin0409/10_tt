4139edf974bd2480e9881e3e539f69cff2f9e396
soomin0409 <55428420+soomin0409@users.noreply.github.com>
2019-11-07 16:07:03 +0900

    05

--- main.c
+++ main.c
@@ -2,28 +2,26 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void swap_1(int *x,int *y)
-{
-	int temp;
-	temp=*x;
-	*x=*y;
-	*y=temp;
-}
-void swap_2(int x, int y){
-	int temp;
-	temp=x;
-	x=y;
-	y=temp;
-}
-
 
 int main(int argc, char *argv[]) {
-	int a=3;
-	int b=5;
-	swap_2(a,b);
-	printf(" a:%i, b:%i\n",a,b);
-	swap_1(&a,&b);
-	printf(" a:%i, b:%i\n",a,b);
+	int i;
+	int grade[5];
+	int sum=0;
+	for(i=0;i<5;i++){
+		printf("input grade[%i]:",i);
+		scanf("%i",&grade[i]);
+	}
+	for(i=0;i<5;i++){
+	
+	printf("grade[%i]: %i\n",i,grade[i]);
+	
+	}
+	int *ptr;
+	ptr=grade;
+	for(i=0;i<5;i++){
+		sum+=*(ptr+i);
+	}
+	printf("average:%i",sum/5);
 	
 	return 0;
 }
