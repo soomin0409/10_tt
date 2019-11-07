e083aba5fb81b94cb021e1e50f42b74630f66cf9
soomin0409 <55428420+soomin0409@users.noreply.github.com>
2019-11-07 15:40:19 +0900

    04

--- main.c
+++ main.c
@@ -2,11 +2,28 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void swap_1(int *x,int *y)
+{
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+}
+void swap_2(int x, int y){
+	int temp;
+	temp=x;
+	x=y;
+	y=temp;
+}
+
+
 int main(int argc, char *argv[]) {
-	int i=4;
-	int *pi = &i;
-	char *pc =&i;
-	printf("%i , %i , %i\n",i,*pi,*pc);
+	int a=3;
+	int b=5;
+	swap_2(a,b);
+	printf(" a:%i, b:%i\n",a,b);
+	swap_1(&a,&b);
+	printf(" a:%i, b:%i\n",a,b);
 	
 	return 0;
 }
